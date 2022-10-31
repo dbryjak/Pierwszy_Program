63ce22080cea93d9fa6df5000db7a06ee911bc09
DanielBryjak <d.bryjak@op.pl>
2022-10-31 20:52:44 +0100

    Druga klasa

---
+++ src/Druga_klasa.java
@@ -0,0 +1,16 @@
+public class Druga_klasa {
+    public static void main(String[] args) {
+    System.out.println("Wynik z drugiej klasy");
+        System.out.println("Dowolny tekst");
+
+        int zmiennaDane;
+        zmiennaDane = 10;
+        System.out.println(zmiennaDane);
+
+        // /*...*/ wyłącza aktywność zawartego pomiędzy tymi znakami
+        /*
+        int zmiennaWartoscX = 5;
+        System.out.println("zmiennaWartoscX");
+         */
+    }
+}
--- src/Środowisko_Programistyczne.java
+++ src/Środowisko_Programistyczne.java
@@ -2,5 +2,6 @@ public class Środowisko_Programistyczne {
     public static void main(String[] args) {
         // IDE - Integrated Development Environment
 
+        System.out.println("Przykładowy tekst");
     }
 }
