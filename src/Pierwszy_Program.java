ce8f3adf21aecaa3a04d50ccc98c772b6d6a788d
DanielBryjak <d.bryjak@op.pl>
2022-10-31 20:14:41 +0100

    Initial commit

---
+++ src/Pierwszy_Program.java
@@ -0,0 +1,6 @@
+public class Pierwszy_Program {
+    public static void main(String[] args) {
+        System.out.println("Witaj Świecie!");
+        System.out.println("Druga linija kodu");
+    }
+}
