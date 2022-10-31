2daf0b36ec73486435bc99c9e96990ee1a4ba233
DanielBryjak <d.bryjak@op.pl>
2022-10-31 21:02:09 +0100

    Tekst wraz z cyframi, oraz tekst wrac z wynikiem dodawania

---
+++ src/Tekst_i_cyfry.java
@@ -0,0 +1,9 @@
+public class Tekst_i_cyfry {
+    public static void main(String[] args) {
+        System.out.println("Wynik dodawania ??? " + 5 + 7);
+        /* + 5 + 7 - zapisuje te liczby tuż koło siebie */
+        System.out.println("Wynik dodawania " + (5 + 7));
+        /* podaje od rasu wynik dodawania*/
+
+    }
+}
