09e83a806363fa1caa73dbbed18d01a246a6f7e5
DanielBryjak <d.bryjak@op.pl>
2022-10-31 21:12:49 +0100

    Tekst wraz z cyframi, oraz tekst wrac z wynikiem dodawania liczb, oraz dodawania zmiennychK

--- src/Tekst_i_cyfry.java
+++ src/Tekst_i_cyfry.java
@@ -2,8 +2,14 @@ public class Tekst_i_cyfry {
     public static void main(String[] args) {
         System.out.println("Wynik dodawania ??? " + 5 + 7);
         /* + 5 + 7 - zapisuje te liczby tuż koło siebie */
-        System.out.println("Wynik dodawania " + (5 + 7));
+        System.out.println("Wynik dodawania: " + (5 + 7));
         /* podaje od rasu wynik dodawania*/
+
+        int mojaZmianna = 222;
+        System.out.println("Moja Zmienna " + mojaZmianna);
+
+        double mojaZmiennaDouble = 13.33;
+        System.out.println("Suma Zmiennych: " + (mojaZmianna + mojaZmiennaDouble));
 
     }
 }
